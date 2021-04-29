44ffb4b306ac29d37ee5b5fb07c704573d3d5af0
Scott Cox <scott.cox@futurenet.com>
2021-04-29 12:12:34 +0100

    Fix linting

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -3,29 +3,43 @@ import store from 'src/state';
 import styled from 'styled-components';
 
 const StyledNav = styled.nav`
-    display: flex;
-    background: blue;
+  display: flex;
+  background: blue;
 
-    a {
-        padding: 0.5rem;
-        color: white;
-    }
-`
+  a {
+    padding: 0.5rem;
+    color: white;
+  }
+`;
 
 export const Navigation = () => (
-    <StyledNav>
-        <Link href="/artists">
-            <a>Artists</a>
-        </Link>
-        <Link href="/write">
-            <a>Write</a>
-        </Link>
-        <Link href="/settings">
-            <a>🔧</a>
-        </Link>
-        <button type="button" onClick={() => { console.log('state', store.getState()) }}>State</button>
-        <button type="button" onClick={() => { localStorage.clear() }}>Clear Local Storage</button>
-    </StyledNav>
-)
+  <StyledNav>
+    <Link href="/artists">
+      <a>Artists</a>
+    </Link>
+    <Link href="/write">
+      <a>Write</a>
+    </Link>
+    <Link href="/settings">
+      <a>🔧</a>
+    </Link>
+    <button
+      type="button"
+      onClick={() => {
+        console.log('state', store.getState());
+      }}
+    >
+      State
+    </button>
+    <button
+      type="button"
+      onClick={() => {
+        localStorage.clear();
+      }}
+    >
+      Clear Local Storage
+    </button>
+  </StyledNav>
+);
 
 export default Navigation;
--- src/libs/artists.ts
+++ src/libs/artists.ts
@@ -1,1 +1,2 @@
-export const getPrimaryArtistImage = (state: API.ArtistImage[]) => state.find((image) => image.type === 'primary');
+export const getPrimaryArtistImage = (state: API.ArtistImage[]) =>
+  state.find((image) => image.type === 'primary');
