f6ff313a7744496ce72067a6a6a29b9011797680
Scott Cox <scott.cox@futurenet.com>
2021-05-10 11:00:05 +0100

    Add theming to ant design

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
+const withLess = require('@zeit/next-less');
+
+module.exports = withLess({
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+  },
   env: {
     PRIMARY_COLOUR: '#f1c40f',
     SECONDARY_COLOUR: '#f39c12i',
   },
-};
+});
